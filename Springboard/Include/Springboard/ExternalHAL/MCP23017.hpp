3fbfa79562eb28d1a69cb5f37bf822c45d739d30
Craig Sacco <craig.sacco@gmail.com>
2017-09-23 14:08:32 +1000

    added MCP23017 low-level driver

---
+++ Springboard/Include/Springboard/ExternalHAL/MCP23017.hpp
@@ -0,0 +1,111 @@
+/******************************************************************************
+ * MIT License
+ *
+ * PROJECT SPRINGBOARD
+ * -------------------
+ * Copyright (c) 2017 <craig.sacco@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *****************************************************************************/
+
+#pragma once
+
+#include <Springboard/InternalHAL/I2CDevice.hpp>
+
+#if SPRINGBOARD_HAL_ENABLE_I2C
+
+namespace Springboard {
+namespace ExternalHAL {
+
+class MCP23017 : public Springboard::InternalHAL::I2CDevice
+{
+public:
+    MCP23017(Springboard::InternalHAL::I2CBus* bus, const Address address,
+             const Speed speed);
+
+    uint16_t ReadIODIRx();
+    uint16_t ReadIPOLx();
+    uint16_t ReadGPINTENx();
+    uint16_t ReadDEFVALx();
+    uint16_t ReadINTCONx();
+    uint8_t ReadIOCON();
+    uint16_t ReadGPPUx();
+    uint16_t ReadINTFx();
+    uint16_t ReadINTCAPx();
+    uint16_t ReadPORTx();
+    uint16_t ReadOLATx();
+    void WriteIODIRx(uint16_t value);
+    void WriteIPOLx(uint16_t value);
+    void WriteGPINTENx(uint16_t value);
+    void WriteDEFVALx(uint16_t value);
+    void WriteINTCONx(uint16_t value);
+    void WriteIOCON(uint8_t value);
+    void WriteGPPUx(uint16_t value);
+    void WriteINTFx(uint16_t value);
+    void WriteINTCAPx(uint16_t value);
+    void WritePORTx(uint16_t value);
+    void WriteOLATx(uint16_t value);
+
+private:
+    enum class Register : uint8_t
+    {
+        IODIRA = 0x00,
+        IODIRB = 0x01,
+        IPOLA = 0x02,
+        IPOLB = 0x03,
+        GPINTENA = 0x04,
+        GPINTENB = 0x05,
+        DEFVALA = 0x06,
+        DEFVALB = 0x07,
+        INTCONA = 0x08,
+        INTCONB = 0x09,
+        IOCON = 0x0A,
+        GPPUA = 0x0C,
+        GPPUB = 0x0D,
+        INTFA = 0x0E,
+        INTFB = 0x0F,
+        INTCAPA = 0x10,
+        INTCAPB = 0x11,
+        PORTA = 0x12,
+        PORTB = 0x13,
+        OLATA = 0x14,
+        OLATB = 0x15,
+    };
+
+    enum class IOCONBits : uint8_t
+    {
+        INTPOL = (1 << 1U),
+        ODR = (1 << 2U),
+        HAEN = (1 << 3U),
+        DISSLW = (1 << 4U),
+        SEQOP = (1 << 5U),
+        MIRROR = (1 << 6U),
+        BANK = (1 << 7U),
+    };
+
+    uint8_t ReadRegister(const Register reg);
+    uint16_t ReadRegisterPair(const Register reg);
+    void WriteRegister(const Register reg, uint8_t value);
+    void WriteRegisterPair(const Register reg, uint16_t value);
+};
+
+}  // namespace ExternalHAL
+}  // namespace Springboard
+
+#endif  // SPRINGBOARD_HAL_ENABLE_I2C
---
+++ Springboard/Source/ExternalHAL/MCP23017.cpp
@@ -0,0 +1,188 @@
+/******************************************************************************
+ * MIT License
+ *
+ * PROJECT SPRINGBOARD
+ * -------------------
+ * Copyright (c) 2017 <craig.sacco@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *****************************************************************************/
+
+#include <Springboard/ExternalHAL/MCP23017.hpp>
+
+#if SPRINGBOARD_HAL_ENABLE_I2C
+
+namespace Springboard {
+namespace ExternalHAL {
+
+MCP23017::MCP23017(Springboard::InternalHAL::I2CBus* bus,
+                   const Address address, const Speed speed)
+    : Springboard::InternalHAL::I2CDevice(bus, address, speed)
+{
+    ASSERT(address >= 0x20 && address <= 0x27);
+}
+
+uint16_t MCP23017::ReadIODIRx()
+{
+    return ReadRegisterPair(Register::IODIRA);
+}
+
+uint16_t MCP23017::ReadIPOLx()
+{
+    return ReadRegisterPair(Register::IPOLA);
+}
+
+uint16_t MCP23017::ReadGPINTENx()
+{
+    return ReadRegisterPair(Register::GPINTENA);
+}
+
+uint16_t MCP23017::ReadDEFVALx()
+{
+    return ReadRegisterPair(Register::DEFVALA);
+}
+
+uint16_t MCP23017::ReadINTCONx()
+{
+    return ReadRegisterPair(Register::INTCONA);
+}
+
+uint8_t MCP23017::ReadIOCON()
+{
+    return ReadRegister(Register::IOCON);
+}
+
+uint16_t MCP23017::ReadGPPUx()
+{
+    return ReadRegisterPair(Register::GPPUA);
+}
+
+uint16_t MCP23017::ReadINTFx()
+{
+    return ReadRegisterPair(Register::INTFA);
+}
+
+uint16_t MCP23017::ReadINTCAPx()
+{
+    return ReadRegisterPair(Register::INTCAPA);
+}
+
+uint16_t MCP23017::ReadPORTx()
+{
+    return ReadRegisterPair(Register::PORTA);
+}
+
+uint16_t MCP23017::ReadOLATx()
+{
+    return ReadRegisterPair(Register::OLATA);
+}
+
+void MCP23017::WriteIODIRx(uint16_t value)
+{
+    return WriteRegisterPair(Register::IODIRA, value);
+}
+
+void MCP23017::WriteIPOLx(uint16_t value)
+{
+    return WriteRegisterPair(Register::IPOLA, value);
+}
+
+void MCP23017::WriteGPINTENx(uint16_t value)
+{
+    return WriteRegisterPair(Register::GPINTENA, value);
+}
+
+void MCP23017::WriteDEFVALx(uint16_t value)
+{
+    return WriteRegisterPair(Register::DEFVALA, value);
+}
+
+void MCP23017::WriteINTCONx(uint16_t value)
+{
+    return WriteRegisterPair(Register::INTCONA, value);
+}
+
+void MCP23017::WriteIOCON(uint8_t value)
+{
+    // only INTPOL, ODR and MIRROR pins are settable
+    static const uint8_t mask = (static_cast<uint8_t>(IOCONBits::INTPOL) |
+                                 static_cast<uint8_t>(IOCONBits::ODR) |
+                                 static_cast<uint8_t>(IOCONBits::MIRROR));
+    return WriteRegister(Register::IOCON, value & mask);
+}
+
+void MCP23017::WriteGPPUx(uint16_t value)
+{
+    return WriteRegisterPair(Register::GPPUA, value);
+}
+
+void MCP23017::WriteINTFx(uint16_t value)
+{
+    return WriteRegisterPair(Register::INTFA, value);
+}
+
+void MCP23017::WriteINTCAPx(uint16_t value)
+{
+    return WriteRegisterPair(Register::INTCAPA, value);
+}
+
+void MCP23017::WritePORTx(uint16_t value)
+{
+    return WriteRegisterPair(Register::PORTA, value);
+}
+
+void MCP23017::WriteOLATx(uint16_t value)
+{
+    return WriteRegisterPair(Register::OLATA, value);
+}
+
+uint8_t MCP23017::ReadRegister(const Register reg)
+{
+    const uint8_t input = static_cast<uint8_t>(reg);
+    uint8_t output;
+    TransmitAndReceive(&input, 1, &output, 1);
+    return output;
+}
+
+uint16_t MCP23017::ReadRegisterPair(const Register reg)
+{
+    const uint8_t input = static_cast<uint8_t>(reg);
+    uint8_t output[2];
+    TransmitAndReceive(&input, 1, output, 2);
+    return output[0] | (static_cast<uint16_t>(output[1]) << 8U);
+}
+
+void MCP23017::WriteRegister(const Register reg, uint8_t value)
+{
+    const uint8_t input[2] = { static_cast<uint8_t>(reg), value };
+    Transmit(input, 2);
+}
+
+void MCP23017::WriteRegisterPair(const Register reg, uint16_t value)
+{
+    const uint8_t input[3] = { static_cast<uint8_t>(reg),
+                               static_cast<uint8_t>(value & 0xff),
+                               static_cast<uint8_t>((value >> 8U) & 0xff) };
+    Transmit(input, 3);
+}
+
+}  // namespace ExternalHAL
+}  // namespace Springboard
+
+#endif  // #if SPRINGBOARD_HAL_ENABLE_I2C
